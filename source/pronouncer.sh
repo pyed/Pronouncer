93bf88a1718919389bbcc745aac781b3750bdf51
pyed <iAbdulelah@Gmail.com>
2014-08-15 04:21:54 +0300

    [Fix] extract the right URL from the new Dictionary.com

--- source/pronouncer.sh
+++ source/pronouncer.sh
@@ -1,7 +1,7 @@
 Url='http://dictionary.reference.com/browse/'$POPCLIP_TEXT
 
 getURL() {
-    curl -s $Url | grep -m1 -oE 'http.+?mp3' | head -1
+    curl -s $Url | grep -m1 -oE 'http.+?mp3' | tail -1
 }
 
 if [[ -s /tmp/$POPCLIP_TEXT.mp3 ]]; then
