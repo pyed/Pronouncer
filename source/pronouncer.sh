d2ed68d84ec091e0d62ddec487299d2f68361942
pyed <iAbdulElah@Gmail.com>
2014-08-16 15:30:21 +0300

    added double quotes to some variables.
    listening to "shellcheck.net"

--- source/pronouncer.sh
+++ source/pronouncer.sh
@@ -1,7 +1,7 @@
 Url='http://dictionary.reference.com/browse/'$POPCLIP_TEXT
 
 getURL() {
-    curl -s $Url | grep -m1 -oE 'http.+?mp3' | tail -1
+    curl -s "$Url" | grep -m1 -oE 'http.+?mp3' | tail -1
 }
 
 if [[ -s /tmp/$POPCLIP_TEXT.mp3 ]]; then
@@ -9,7 +9,7 @@ if [[ -s /tmp/$POPCLIP_TEXT.mp3 ]]; then
 else
     extractedURL=$(getURL)
     if [[ -n $extractedURL ]]; then
-        curl -s -o "/tmp/$POPCLIP_TEXT.mp3" $extractedURL
+        curl -s -o "/tmp/$POPCLIP_TEXT.mp3" "$extractedURL"
         afplay "/tmp/$POPCLIP_TEXT.mp3"
     else
         exit 1
